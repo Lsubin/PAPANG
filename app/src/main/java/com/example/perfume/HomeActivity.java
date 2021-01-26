0123433cb179f73893390df4be06e1d55d939e39
khedmsl1028 <khedmsl1028@daum.net>
2021-01-27 01:38:17 +0900

    First
    First

---
+++ app/src/main/java/com/example/perfume/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.perfume;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ConstraintLayout home = (ConstraintLayout)findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/perfume/main/my/MyFragment.java
@@ -0,0 +1,20 @@
+package com.example.perfume.main.my;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.perfume.R;
+
+public class MyFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_my, container, false);
+        return root;
+    }
+}
