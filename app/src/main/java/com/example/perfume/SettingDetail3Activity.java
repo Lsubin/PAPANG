013f72c1ae4d2c2c71a74e165421371be9aeb48a
Lsubin <bin522@naver.com>
2021-05-13 22:44:00 +0900

    [설정] 웹뷰 연결
    웹뷰 이용해서 노션(이용약관, 개인정보처리방침)과 연결

--- app/src/main/java/com/example/perfume/SettingDetail3Activity.java
+++ app/src/main/java/com/example/perfume/SettingDetail3Activity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 
 // 개인정보처리방침
 public class SettingDetail3Activity extends AppCompatActivity {
 
     private ImageButton btn_back;
+    private WebView mWebView; // 웹뷰 선언
+    private WebSettings mWebSettings; //웹뷰세팅
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +29,24 @@ public class SettingDetail3Activity extends AppCompatActivity {
                 finish();
             }
         });
+
+        // 웹뷰 시작
+        mWebView = (WebView) findViewById(R.id.webView);
+
+        mWebView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
+        mWebView.setWebChromeClient(new WebChromeClient()); // 클릭시 새창 안뜨게
+        mWebSettings = mWebView.getSettings(); //세부 세팅 등록
+        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
+        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
+        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
+        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+
+        mWebView.loadUrl("https://www.notion.so/PAPANG-397158d96af34f18ac94e09a093f3289"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
     }
 }
